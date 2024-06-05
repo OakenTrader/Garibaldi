fa6f9842f2df4f93a598f95a99042ad379ca520b
OakenTrader <oakentrader@gmail.com>
2024-06-05 20:10:32 +0700

    Added retrieve_from_tree() and type hinting for functions

--- utility.py
+++ utility.py
@@ -1,10 +1,13 @@
 import json, sys
 
-def jopen(address):
+def jopen(address:str):
     with open(address) as file:
         return json.load(file)
 
-def get_all_possible_keys(data):
+def get_all_possible_keys(data:dict):
+    """
+    Retrieve a list of all unique keys of member dictionaries in a dictionary.
+    """
     unique_keys = set()
     for k, v in data.items():
         if isinstance(v, dict):
@@ -27,3 +30,26 @@ def get_size(obj, seen=None):
     elif isinstance(obj, (list, tuple, set, frozenset)):
         size += sum([get_size(i, seen) for i in obj])
     return size
+
+def retrieve_from_tree(tree:dict, directory:list):
+    """
+    Retrieve a value from a nested dictionary (tree) using a list of keys (directory).
+
+    Args:
+        tree (dict): The nested dictionary to retrieve the value from.
+        directory (list): A list of keys representing the path to the desired value.
+
+    Returns:
+        The value found at the specified directory path, or None if any key in the path does not exist in the tree.
+
+    Example:
+        tree = {'a': {'b': {'c': 'value'}}}
+        directory = ['a', 'b', 'c']
+        result = retrieve_from_tree(tree, directory)  # result will be 'value'
+    """
+    current = tree
+    for subdir in directory:
+        if subdir not in current:
+            return None
+        current = tree[subdir]
+    return current
