75203881d7b78c804f9d4aebb11bca6ee4bc4d14
OakenTrader <oakentrader@gmail.com>
2024-10-17 22:57:53 +0200

    feat(checker): :sparkles: Added table for missing technologies of each player
    - The checker now creates a table of missing techs to compare tech by tech between players
    - Added utf-8 encoding specifications
    - Changed naming from notable_countries to player_countries
    - changed .txt to .csv at output specification

--- scripts/checkers/check_tech.py
+++ scripts/checkers/check_tech.py
@@ -9,7 +9,7 @@ class CheckTech(Checker):
     """
 
     requirements = ["country_manager", "technology"]
-    output = {"tech_tree.txt": ["tech_tree"]}
+    output = {"tech_tree.csv": ["tech_tree"], "tech_table.csv": ["tech_table"]}
 
     def __init__(self):
         super().__init__()
@@ -57,7 +57,8 @@ class CheckTech(Checker):
         output += f"{frontier}\n"
 
         """Who is researching which"""
-        notable_countries = [p[0] for p in players] # get country_id
+        player_countries = [str(p[0]) for p in players] # get country_id
+        missing_techs = set()
         df_tech = []
         for tech_id, tech_entry in technologies.items():
             if not isinstance(tech_entry, dict):
@@ -69,22 +70,44 @@ class CheckTech(Checker):
             country_tag = countries[country_id]["definition"]
             country_name = get_country_name(countries[country_id], localization)
 
-            if researching_tech in frontier or int(country_id) in notable_countries:
+            if researching_tech in frontier or country_id in player_countries:
                 output += f"{tech_id} {country_tag} {country_name} : {researching_tech}\n"
                 his_tech = technologies[tech_id]["acquired_technologies"]["value"]
                 his_prod_tech = [tech for tech in his_tech if tech in def_prod_tech]
                 his_mil_tech = [tech for tech in his_tech if tech in def_mil_tech]
                 his_soc_tech = [tech for tech in his_tech if tech in def_soc_tech]
-                output += f"Number of tech: {len(his_tech)}, {[len(his_prod_tech), len(his_mil_tech), len(his_soc_tech)]}\n"
+                num_prod_tech = len(his_prod_tech)
+                num_mil_tech = len(his_mil_tech)
+                num_soc_tech = len(his_soc_tech)
+                if num_prod_tech + num_mil_tech + num_soc_tech < len(his_tech):
+                    raise ValueError("Some techs not in definitions, likely due to missing mod specification")
+                output += f"Number of tech: {len(his_tech)}, {[num_prod_tech, num_mil_tech, num_soc_tech]}\n"
                 his_missing_tech = [tech for tech in techs if tech not in his_tech]
+                countries[country_id]["Missing tech"] = his_missing_tech
+                missing_techs.update(set(his_missing_tech))
                 output += "Missing tech\n"
                 output += f"{len(his_missing_tech)}, {his_missing_tech}\n\n"
-                df_tech.append({"id": country_id, "tag": country_tag, "country": country_name, "production techs":len(his_prod_tech), "military techs":len(his_mil_tech), "society techs":len(his_soc_tech), "total techs":len(his_tech)})
+                df_tech.append({"id": country_id, "tag": country_tag, "country": country_name, "production techs":num_prod_tech, "military techs":num_mil_tech, "society techs":num_soc_tech, "total techs":len(his_tech)})
+
+        df_missing_techs = []
+        for country_id in player_countries:
+            if not isinstance(country := retrieve_from_tree(countries, [country_id]), dict):
+                continue
+            df_missing_tech = {"id":country_id, "tag":country["definition"], "country":get_country_name(country, localization)}
+            df_missing_tech.update({tech:False for tech in missing_techs})
+            missing_tech = countries[country_id]["Missing tech"]
+            for tech in missing_techs:
+                if tech in missing_tech:
+                    df_missing_tech[tech] = True
+            df_missing_techs.append(df_missing_tech)
+        df_missing_techs = pd.DataFrame(df_missing_techs, columns=["id", "tag", "country"] + list(missing_techs))
+        """TODO Make missing tech columns sorted according to category and eras"""
 
         df_tech = pd.DataFrame(df_tech, columns=["id", "tag", "country", "production techs", "military techs", "society techs", "total techs"])
         with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            df_tech.to_csv(f"{address}/tech_tree.csv", sep=",", index=False)
-            with open(f"{address}/tech_tree.txt", "w") as file:
+            df_tech.to_csv(f"{address}/tech_tree.csv", sep=",", index=False, encoding="utf-8")
+            df_missing_techs.to_csv(f"{address}/tech_table.csv", sep=",", index=False, encoding="utf-8")
+            with open(f"{address}/tech_tree.txt", "w", encoding="utf-8") as file:
                 year, month, day = save_date
                 file.write(f"{day}/{month}/{year}\n")
                 file.write(output)
