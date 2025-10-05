22108f3bf50d6fccf264b1685a8e19362b911597
Max <Maximilianlakke@googlemail.com>
2025-10-05 19:08:11 +0200

    add pyodide as python executer

---
+++ web/src/components/PythonExecute.vue
@@ -0,0 +1,24 @@
+<script>
+import { loadPyodide } from 'pyodide'
+
+export default {
+  async created() {
+    this.pyodide = await loadPyodide()
+  },
+  methods: {
+    async runPython() {
+      // kein loadPackage - stattdessen einfach pyodide benutzen
+      const code = `
+    print("Hello from Pyodide!")
+    # Add your Python code here
+  `
+      const result = await this.pyodide.runPythonAsync(code)
+      console.log(result)
+    },
+  },
+}
+</script>
+
+<template>
+  <button @click="runPython">Run Python</button>
+</template>
